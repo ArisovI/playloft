49bd5ad87559525ba4aa30a29c39d52d6c75d142
Ilkhambek <cool.ilxam@gmail.com>
2024-01-02 20:26:50 +0500

    slidesPrev

--- src/components/Platforms/index.tsx
+++ src/components/Platforms/index.tsx
@@ -7,7 +7,7 @@ const index = () => {
   return (
     <div className="container">
       <div className="mt-[65px]">
-        <Swiper slidesPerView={5} loop>
+        <Swiper slidesPerView={4} loop>
           <SwiperSlide>
             <div className="flex flex-col gap-[35px] items-center justify-center w-[273px] h-[251px] rounded-[22px] shadow-platform">
               <img src={Icons.type1} alt="" />
